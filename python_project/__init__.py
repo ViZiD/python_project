c1c13aa7a85ed876f05f5b9fc06cd8eb6646ed20
Radik Islamov <vizid1337@gmail.com>
2024-07-19 00:42:26 +0500

    fix mypy typing

--- python_project/__init__.py
+++ python_project/__init__.py
@@ -27,7 +27,8 @@ async def command_start_handler(message: Message) -> None:
     # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
     # method automatically or call API method directly via
     # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+    msg = getattr(message.from_user, "full_name", "")
+    await message.answer(f"Hello, {html.bold(msg)}!")
 
 
 @dp.message()
