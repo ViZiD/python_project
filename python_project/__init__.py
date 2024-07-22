21c95fd180fd1eff0763202daabd5bd2109c27ec
Radik Islamov <vizid1337@gmail.com>
2024-07-22 08:45:22 +0500

    fix script

--- python_project/__init__.py
+++ python_project/__init__.py
@@ -54,6 +54,6 @@ async def main() -> None:
     await dp.start_polling(bot)
 
 
-if __name__ == "__main__":
+def run():
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
